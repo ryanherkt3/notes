c9b1fa1984015df36aac6be1c27cb2c77aa0fe09
Ryan Herkt <ryanherkt@gmail.com>
2025-07-06 22:16:03 +1200

    Add basic testing to validate saving notes to DB

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save note with no title or content" do
+    note = Note.new
+    assert_not note.save
+  end
+
+  test "should not save note with no title" do
+    note = Note.new(content: "hi there")
+    assert_not note.save
+  end
+
+  test "should not save note with no content" do
+    note = Note.new(title: "hello")
+    assert_not note.save
+  end
+
+  test "should save note" do
+    note = Note.new(title: "hello", content: "hi there")
+    assert note.save
+  end
 end
